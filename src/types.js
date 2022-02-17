064fa4076b146b239fb1692cac2d9ae71752e971
Lemon <riegoshinandrei@gmail.com>
2022-02-17 12:58:34 +0800

    chore: Create basic types for Hooks

--- src/types.js
+++ src/types.js
@@ -13,3 +13,21 @@
  * @property {Boolean} [remove=false] - whether to remove the attr after execution
  * @property {DataObject} - the data
  */
+
+/**
+ * @typedef {Object} HookData
+ * @property {string} prop - the object property that will be observed; changing this prop's value will trigger the hook.
+ * @property {any} value - the value of the object[prop]
+ * @property {?Function} trap - the callback that will be called on the value
+ */
+
+/**
+ * @typedef {Object} Hook
+ * @property {HookData} data
+ */
+
+/**
+ * @callback HookFunction
+ * @param {?Function} trap - the callback to be called on the hook value
+ * @returns {Hook}
+ */
