6201a674f684ddf1cdc98787c7ae022c343c04aa
Lemon <riegoshinandrei@gmail.com>
2022-03-14 13:52:07 +0800

    test: Update tests for lifecycle

--- src/test/lifecycle.test.js
+++ src/test/lifecycle.test.js
@@ -1,93 +1,50 @@
 import '@testing-library/jest-dom/extend-expect';
 import { html, render } from '..';
 
-describe('lifecycle methods', () => {
-  // eslint-disable-next-line
-  let thisFunction, arrowFunction;
-  const createDiv = (children, method, cb) =>
-    html`<div ${{ [`on${method}`]: cb }}>${children}</div>`;
+/**
+ * Notes:
+ * - With the exception of `create`, the rest needs to be tested asynchronously
+ * - Due to the asynchronous nature of mutation observer, each test div has to be localized
+ *   to avoid messing with other tests
+ */
 
-  beforeEach(() => {
-    thisFunction = jest.fn(function () {
-      return this;
-    });
-    arrowFunction = jest.fn((node) => node);
-  });
+describe('lifecycle methods', () => {
+  const onCreate = jest.fn((e) => e);
+  const onDestroy = jest.fn((e) => e);
+  const onMount = jest.fn((e) => e);
+  const onUnmount = jest.fn((e) => e);
 
   afterEach(() => {
     document.body.innerHTML = '';
+    jest.clearAllMocks();
   });
 
   describe('@create', () => {
-    // eslint-disable-next-line
-    let div1, div2;
-    beforeEach(() => {
-      div1 = createDiv('This is my div', 'create', thisFunction);
-      div2 = createDiv(
-        createDiv(
-          createDiv('', 'create', thisFunction),
-          'create',
-          thisFunction
-        ),
-        'create',
-        thisFunction
-      );
-    });
+    const div = html`<div onCreate=${onCreate}>Hello, World!</div>`;
 
     it('runs on element creation', () => {
-      render(div1);
-      expect(thisFunction).toHaveBeenCalledTimes(1);
-    });
-
-    it('runs recursively', () => {
-      render(div2);
-      expect(thisFunction).toHaveBeenCalledTimes(3);
+      render(div);
+      expect(onCreate).toHaveBeenCalledTimes(1);
     });
 
     it('runs only once', () => {
-      const div = render(div1);
-      div.dispatchEvent(new Event('@create'));
-      div.dispatchEvent(new Event('@create'));
+      const el = render(div);
+      el.dispatchEvent(new Event('@create'));
+      el.dispatchEvent(new Event('@create'));
 
-      expect(thisFunction).toHaveBeenCalledTimes(1);
-    });
-
-    it('has reference to the element it was attached to', () => {
-      const el = render(div1);
-
-      expect(thisFunction).toHaveReturnedWith(el.firstChild);
-    });
-
-    it('node is passed to the callback', () => {
-      const el = render(createDiv('', 'create', arrowFunction));
-
-      expect(arrowFunction.mock.results[0].value.target).toBe(el.firstChild);
+      expect(onCreate).toHaveBeenCalledTimes(1);
     });
   });
 
   describe('@destroy', () => {
-    // eslint-disable-next-line
-    let div1, div2;
-    beforeEach(() => {
-      div1 = createDiv('This is my div', 'destroy', thisFunction);
-      div2 = createDiv(
-        createDiv(
-          createDiv('', 'destroy', thisFunction),
-          'destroy',
-          thisFunction
-        ),
-        'destroy',
-        thisFunction
-      );
-    });
+    const div = html`<div onDestroy=${onDestroy}>Hello, World!</div>`;
 
     it('runs when element is destroyed', (done) => {
-      const el = render(div1, 'body');
-      el.firstChild.remove();
+      render(div, 'body').firstElementChild.remove();
 
       setTimeout(() => {
         try {
-          expect(thisFunction).toHaveBeenCalledTimes(1);
+          expect(onDestroy).toHaveBeenCalledTimes(1);
           done();
         } catch (error) {
           done(error);
@@ -96,59 +53,14 @@ describe('lifecycle methods', () => {
     });
 
     it('does not run when node is moved', (done) => {
-      const el = render(div1, 'body');
-      const test = render(html`<div id="test"></div>`).firstChild;
-      document.body.append(test);
-      test.append(el.firstChild);
+      const el = render(div, 'body').firstElementChild;
+      const extraEl = render(html`<div></div>`).firstElementChild;
+      document.body.append(extraEl);
+      extraEl.append(el);
 
       setTimeout(() => {
         try {
-          expect(thisFunction).not.toHaveBeenCalled();
-          done();
-        } catch (error) {
-          done(error);
-        }
-      }, 0);
-    });
-
-    it('runs recursively', (done) => {
-      const el = render(div2, 'body');
-      const child = el.firstChild;
-      child.remove();
-
-      setTimeout(() => {
-        try {
-          expect(thisFunction).toHaveBeenCalledTimes(3);
-          done();
-        } catch (error) {
-          done(error);
-        }
-      }, 0);
-    });
-
-    it('has reference to element it was attached to', (done) => {
-      const el = render(div1, 'body');
-      const child = el.firstChild;
-      child.remove();
-
-      setTimeout(() => {
-        try {
-          expect(thisFunction).toHaveReturnedWith(child);
-          done();
-        } catch (error) {
-          done(error);
-        }
-      }, 0);
-    });
-
-    it('node is passed to the callback', (done) => {
-      const el = render(createDiv('', 'destroy', arrowFunction), 'body');
-      const child = el.firstChild;
-      child.remove();
-
-      setTimeout(() => {
-        try {
-          expect(arrowFunction.mock.results[0].value.target).toBe(child);
+          expect(onDestroy).not.toHaveBeenCalled();
           done();
         } catch (error) {
           done(error);
@@ -158,23 +70,14 @@ describe('lifecycle methods', () => {
   });
 
   describe('@mount', () => {
-    // eslint-disable-next-line
-    let div1, div2;
-    beforeEach(() => {
-      div1 = createDiv('This is my div', 'mount', thisFunction);
-      div2 = createDiv(
-        createDiv(createDiv('', 'mount', thisFunction), 'mount', thisFunction),
-        'mount',
-        thisFunction
-      );
-    });
+    const div = html`<div onMount=${onMount}>Hello, World!</div>`;
 
     it('runs on mount', (done) => {
-      render(div1, 'body');
+      render(div, 'body');
 
       setTimeout(() => {
         try {
-          expect(thisFunction).toHaveBeenCalledTimes(1);
+          expect(onMount).toHaveBeenCalledTimes(1);
           done();
         } catch (error) {
           done(error);
@@ -183,57 +86,14 @@ describe('lifecycle methods', () => {
     });
 
     it('runs when node is moved', (done) => {
-      const el = render(div1, 'body').firstChild;
-      const test = render(html`<div id="test"></div>`, 'body');
-      setTimeout(() => {
-        test.append(el);
-      }, 0);
+      const el = render(div, 'body').firstElementChild;
+      render(html`<div id="test"></div>`, 'body');
+
+      setTimeout(() => document.getElementById('test').append(el), 0);
 
       setTimeout(() => {
         try {
-          expect(thisFunction).toHaveBeenCalledTimes(2);
-          done();
-        } catch (error) {
-          done(error);
-        }
-      }, 0);
-    });
-
-    it('runs recursively', (done) => {
-      render(div2, 'body');
-
-      setTimeout(() => {
-        try {
-          expect(thisFunction).toHaveBeenCalledTimes(3);
-          done();
-        } catch (error) {
-          done(error);
-        }
-      }, 0);
-    });
-
-    it('has reference to element it was attached to', (done) => {
-      const el = render(div1, 'body');
-
-      setTimeout(() => {
-        try {
-          expect(thisFunction).toHaveReturnedWith(el.firstChild);
-          done();
-        } catch (error) {
-          done(error);
-        }
-      }, 0);
-    });
-
-    it('node is passed to the callback', (done) => {
-      const el = render(createDiv('', 'mount', arrowFunction), 'body');
-
-      setTimeout(() => {
-        try {
-          expect(arrowFunction.mock.results[0].value.target).toBe(
-            el.firstChild
-          );
-
+          expect(onMount).toHaveBeenCalledTimes(2);
           done();
         } catch (error) {
           done(error);
@@ -243,28 +103,14 @@ describe('lifecycle methods', () => {
   });
 
   describe('@unmount', () => {
-    // eslint-disable-next-line
-    let div1, div2;
-    beforeEach(() => {
-      div1 = createDiv('This is my div', 'unmount', thisFunction);
-      div2 = createDiv(
-        createDiv(
-          createDiv('', 'unmount', thisFunction),
-          'unmount',
-          thisFunction
-        ),
-        'unmount',
-        thisFunction
-      );
-    });
+    const div = html`<div onUnmount=${onUnmount}>Hello, World!</div>`;
 
     it('runs when element is destroyed', (done) => {
-      const el = render(div1, 'body');
-      el.firstChild.remove();
+      render(div, 'body').firstElementChild.remove();
 
       setTimeout(() => {
         try {
-          expect(thisFunction).toHaveBeenCalledTimes(1);
+          expect(onUnmount).toHaveBeenCalledTimes(1);
           done();
         } catch (error) {
           done(error);
@@ -273,58 +119,14 @@ describe('lifecycle methods', () => {
     });
 
     it('runs when node is moved', (done) => {
-      const el = render(div1, 'body').firstChild;
-      const test = render(html`<div id="test"></div>`).firstChild;
-      document.body.append(test);
-      test.append(el);
+      const el = render(div, 'body').firstElementChild;
+      const extraEl = render(html`<div></div>`).firstElementChild;
+      document.body.append(extraEl);
+      extraEl.append(el);
 
       setTimeout(() => {
         try {
-          expect(thisFunction).toHaveBeenCalledTimes(1);
-          done();
-        } catch (error) {
-          done(error);
-        }
-      }, 0);
-    });
-
-    it('runs recursively', (done) => {
-      const el = render(div2, 'body');
-      el.firstChild.remove();
-
-      setTimeout(() => {
-        try {
-          expect(thisFunction).toHaveBeenCalledTimes(3);
-          done();
-        } catch (error) {
-          done(error);
-        }
-      }, 0);
-    });
-
-    it('has reference to element it was attached to', (done) => {
-      const el = render(div1, 'body');
-      const child = el.firstChild;
-      child.remove();
-
-      setTimeout(() => {
-        try {
-          expect(thisFunction).toHaveReturnedWith(child);
-          done();
-        } catch (error) {
-          done(error);
-        }
-      }, 0);
-    });
-
-    it('node is passed to the callback', (done) => {
-      const el = render(createDiv('', 'unmount', arrowFunction), 'body');
-      const child = el.firstChild;
-      child.remove();
-
-      setTimeout(() => {
-        try {
-          expect(arrowFunction.mock.results[0].value.target).toBe(child);
+          expect(onUnmount).toHaveBeenCalledTimes(1);
           done();
         } catch (error) {
           done(error);
@@ -333,15 +135,34 @@ describe('lifecycle methods', () => {
     });
   });
 
-  it('allows multiple callbacks for single type', (done) => {
-    const el = render(createDiv('', 'destroy', thisFunction), 'body');
-    el.firstChild.addEventListener('@destroy', arrowFunction);
-    el.firstChild.remove();
+  it('run recursively', () => {
+    render(html`<div><div onCreate=${onCreate}>Hello, World!</div></div>`);
+
+    expect(onCreate).toHaveBeenCalledTimes(1);
+  });
+
+  it('pass the element as e.target to the callback', () => {
+    const fragment = render(
+      html`<div onCreate=${onCreate}>Hello, World!</div>`
+    );
+
+    expect(onCreate.mock.results[0].value.target).toBe(
+      fragment.firstElementChild
+    );
+  });
+
+  it('can have multiple callbacks for a type', (done) => {
+    const div = html`<div onDestroy=${onDestroy}>Hello, World!</div>`;
+    const el = render(div, 'body').firstElementChild;
+    const mock = jest.fn();
+
+    el.addEventListener('@destroy', mock);
+    el.remove();
 
     setTimeout(() => {
       try {
-        expect(thisFunction).toHaveBeenCalledTimes(1);
-        expect(arrowFunction).toHaveBeenCalledTimes(1);
+        expect(onDestroy).toHaveBeenCalledTimes(1);
+        expect(mock).toHaveBeenCalledTimes(1);
         done();
       } catch (error) {
         done(error);
