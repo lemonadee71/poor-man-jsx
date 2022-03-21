4303c7f33345f026a0e135aaf70be4ac244367cc
Lemon <riegoshinandrei@gmail.com>
2022-03-21 12:34:23 +0800

    test: Update tests for naive diffing

--- src/test/diff.test.js
+++ src/test/diff.test.js
@@ -1,140 +1,228 @@
+import '@testing-library/jest-dom/extend-expect';
 import { screen } from '@testing-library/dom';
 import { createHook, html, render } from '..';
-import '@testing-library/jest-dom/extend-expect';
-
-const createTodoItem = (mountCallback, unmountCallback) => (data) =>
-  html`
-    <div class="todo" ${data.keyString}="${data.id}">
-      <h2
-        class="todo__title"
-        onMount=${mountCallback}
-        onUnmount=${unmountCallback}
-      >
-        ${data.name}
-      </h2>
-    </div>
-  `;
-
-const createTodoList = (defaultData, Todo, keyString = 'key') => {
-  const [data] = createHook({ todos: defaultData });
-  const component = html`
-    <h2>This is my Todo</h2>
-    <div is-list data-testid="todo-list" keystring="${keyString}">
-      ${data.$todos
-        .map((todo) => Todo({ ...todo, keyString }))
-        .map((item) => render(item))}
-    </div>
-  `;
-
-  return { data, component };
-};
 
 const getKeys = (node) =>
   [...node.children]
     .map((child) => child.getAttribute(node.getAttribute('keystring') || 'key'))
     .join();
 
-const getTitle = (node) =>
-  [...node.children]
-    .map((child) => child.querySelector('.todo__title').textContent.trim())
-    .join();
+const getInnerText = (node) =>
+  [...node.children].map((child) => child.textContent.trim()).join();
+
+const ListItem = (data, props = {}) =>
+  render(html`
+    <li id="${data.id}" ${{ [props.keyString || 'key']: data.id, ...props }}>
+      ${data.text}
+    </li>
+  `);
 
 describe('diffing', () => {
+  const onMount = jest.fn();
+  const onUnmount = jest.fn();
+
   const defaultData = [
-    { id: 'eat', name: 'eat' },
-    { id: 'shower', name: 'shower' },
-    { id: 'sleep', name: 'sleep' },
+    { id: '1', text: 'eat' },
+    { id: '2', text: 'shower' },
+    { id: '3', text: 'sleep' },
   ];
 
-  let mountCallback, unmountCallback, defaultTodo; //eslint-disable-line
   beforeEach(() => {
-    mountCallback = jest.fn();
-    unmountCallback = jest.fn();
-    defaultTodo = createTodoItem(mountCallback, unmountCallback);
+    // to make sure mocks are cleared before each test
+    // since mutation observer batches changes
+    // and might trigger lifecycle even if the test was finished
+    jest.clearAllMocks();
   });
 
   afterEach(() => {
     document.body.innerHTML = '';
+    jest.clearAllMocks();
   });
 
-  it('shuffles the children efficiently', (done) => {
-    const { data, component } = createTodoList(defaultData, defaultTodo);
-    render(component, 'body');
-
-    data.todos = [
-      { id: 'shower', name: 'shower' },
-      { id: 'eat', name: 'eat' },
-      { id: 'sleep', name: 'sleep' },
-    ];
+  it('reflect changes in order', (done) => {
+    const [hook] = createHook({ list: defaultData });
+    render(
+      html`<ul is-list data-testid="shuffled">
+        ${hook.$list.map((data) => ListItem(data, { onUnmount }))}
+      </ul>`,
+      'body'
+    );
 
     setTimeout(() => {
-      try {
-        expect(getKeys(screen.getByTestId('todo-list'))).toBe(
-          data.todos.map((todo) => todo.id).join()
-        );
-        expect(mountCallback).toHaveBeenCalledTimes(4);
-        expect(unmountCallback).toHaveBeenCalledTimes(1);
-        done();
-      } catch (error) {
-        done(error);
-      }
-    }, 0);
-  });
-
-  it('adds new children', (done) => {
-    const { data, component } = createTodoList(defaultData, defaultTodo);
-    render(component, 'body');
-
-    data.todos = [
-      { id: 'eat', name: 'eat' },
-      { id: 'exercise', name: 'exercise' },
-      { id: 'shower', name: 'shower' },
-      { id: 'sleep', name: 'sleep' },
-      { id: 'read', name: 'read' },
-    ];
-
-    setTimeout(() => {
-      try {
-        expect(getKeys(screen.getByTestId('todo-list'))).toBe(
-          data.todos.map((todo) => todo.id).join()
-        );
-        expect(mountCallback).toHaveBeenCalledTimes(7);
-        expect(unmountCallback).not.toHaveBeenCalled();
-        done();
-      } catch (error) {
-        done(error);
-      }
-    }, 0);
-  });
-
-  it('throws an error if child has no key', () => {
-    const { data, component } = createTodoList(defaultData, defaultTodo);
-    render(component, 'body');
-
-    expect(() => {
-      data.todos = [
-        { id: 'exercise', name: 'exercise' },
-        { id: 'sleep', name: 'sleep' },
-        { id: '', name: 'eat' },
-        { id: 'read', name: 'read' },
-        { id: 'shower', name: 'shower' },
+      hook.list = [
+        { id: '2', text: 'shower' },
+        { id: '1', text: 'eat' },
+        { id: '3', text: 'sleep' },
       ];
-    }).toThrowError(
-      'every children should have a key if parent is of type list'
+    });
+
+    setTimeout(() => {
+      try {
+        expect(getKeys(screen.getByTestId('shuffled'))).toBe(
+          hook.list.map((item) => item.id).join()
+        );
+        expect(onUnmount).toBeCalledTimes(1);
+        done();
+      } catch (error) {
+        done(error);
+      }
+    }, 0);
+  });
+
+  it('adds new items', (done) => {
+    const [hook] = createHook({ list: defaultData });
+    render(
+      html`<ul is-list data-testid="added">
+        ${hook.$list.map((data) => ListItem(data, { onMount, onUnmount }))}
+      </ul>`,
+      'body'
+    );
+
+    setTimeout(() => {
+      hook.list = [
+        { id: '1', text: 'eat' },
+        { id: '4', text: 'exercise' },
+        { id: '2', text: 'shower' },
+        { id: '3', text: 'sleep' },
+        { id: '5', text: 'read' },
+      ];
+    });
+
+    setTimeout(() => {
+      try {
+        expect(getKeys(screen.getByTestId('added'))).toBe(
+          hook.list.map((item) => item.id).join()
+        );
+        expect(onUnmount).not.toBeCalled();
+        expect(onMount).toBeCalledTimes(5);
+        done();
+      } catch (error) {
+        done(error);
+      }
+    }, 0);
+  });
+
+  it('only updates what was changed', () => {
+    const [hook] = createHook({ list: defaultData });
+    render(
+      html`<div is-list data-testid="update">
+        ${hook.$list.map((data) =>
+          render(html`<p key="${data.id}">${data.text}</p>`)
+        )}
+      </div>`,
+      'body'
+    );
+
+    hook.list = [
+      { id: '1', text: 'eating' },
+      { id: '2', text: 'shower' },
+      { id: '3', text: 'sleep' },
+    ];
+
+    expect(getInnerText(screen.getByTestId('update'))).toBe(
+      hook.list.map((item) => item.text).join()
     );
   });
 
-  it('accepts other key string', () => {
-    const { data, component } = createTodoList(defaultData, defaultTodo, 'id');
-    render(component, 'body');
+  it('updates "non-text" elements if it has attribute `is-text`', () => {
+    const [hook] = createHook({ list: defaultData });
+    render(
+      html`<ul is-list data-testid="is-text">
+        ${hook.$list.map((data) => ListItem(data, { 'is-text': '' }))}
+      </ul>`,
+      'body'
+    );
 
-    expect(getKeys(screen.getByTestId('todo-list'))).toBe(
-      data.todos.map((todo) => todo.id).join()
+    hook.list = [
+      { id: '1', text: 'eating' },
+      { id: '2', text: 'shower' },
+      { id: '3', text: 'sleep' },
+    ];
+
+    expect(getInnerText(screen.getByTestId('is-text'))).toBe(
+      hook.list.map((item) => item.text).join()
+    );
+  });
+
+  it('it is applied recursively', (done) => {
+    const tags = new Array(3).fill().map((_, i) => `Tag ${i + 1}`);
+    const [hook] = createHook({ list: [{ id: '1', text: 'eat', tags }] });
+    const createContent = (data) =>
+      render(html`
+        <ul is-list data-testid="nested-list">
+          ${(data.tags || []).map(
+            (item) =>
+              html`<li key="${item}" onMount=${onMount} onUnmount=${onUnmount}>
+                ${item}
+              </li>`
+          )}
+        </ul>
+        <p>${data.text}</p>
+      `);
+
+    render(
+      html`<ul is-list>
+        ${hook.$list.map((data) =>
+          ListItem(data, { onMount, onUnmount, children: createContent(data) })
+        )}
+      </ul>`,
+      'body'
+    );
+
+    setTimeout(() => {
+      hook.list = [{ id: '1', text: 'eat', tags: tags.slice(1) }];
+    });
+
+    setTimeout(() => {
+      try {
+        expect(getInnerText(screen.getByTestId('nested-list'))).toBe(
+          hook.list[0].tags.join()
+        );
+        expect(onUnmount).toBeCalledTimes(1);
+        expect(onMount).toBeCalledTimes(4);
+        done();
+      } catch (error) {
+        done(error);
+      }
+    }, 0);
+  });
+
+  it('throws an error if a child has no key', () => {
+    const [hook] = createHook({ list: defaultData });
+    render(
+      html`<ul is-list>
+        ${hook.$list.map((data) => ListItem(data))}
+      </ul>`,
+      'body'
+    );
+
+    expect(() => {
+      hook.list = [
+        { id: '1', text: 'eat' },
+        { id: '4', text: 'exercise' },
+        { id: '', text: 'shower' },
+      ];
+    }).toThrow();
+  });
+
+  it('accepts other key string', () => {
+    const [hook] = createHook({ list: defaultData });
+    render(
+      html`<ul is-list keystring="id" data-testid="keystring">
+        ${hook.$list.map((data) => ListItem(data, { keyString: 'id' }))}
+      </ul>`,
+      'body'
+    );
+
+    hook.list = [...defaultData].reverse();
+
+    expect(getKeys(screen.getByTestId('keystring'))).toBe(
+      hook.list.map((item) => item.id).join()
     );
   });
 
   it('does not update ignored attributes', () => {
-    const [data] = createHook(1);
+    const [hook] = createHook(1);
     const child = (i) => {
       const [current] = createHook(Math.random());
 
@@ -152,7 +240,7 @@ describe('diffing', () => {
     };
     const component = html`
       <div is-list>
-        ${data.$value((n) =>
+        ${hook.$value((n) =>
           new Array(n).fill().map((_, i) => render(child(i)))
         )}
       </div>
@@ -160,10 +248,10 @@ describe('diffing', () => {
     render(component, 'body');
 
     const el = screen.getByTestId('test-child-0');
-    const proxyId = el.getAttribute('data-proxyid');
+    const proxyId = el.dataset.proxyid;
     const dataId = el.dataset.id;
 
-    data.value = 2;
+    hook.value = 2;
 
     expect(el.getAttribute('data-proxyid')).toBe(proxyId);
     expect(el.getAttribute('data-id')).toBe(dataId);
@@ -171,218 +259,54 @@ describe('diffing', () => {
   });
 
   it('does not remove ignored attributes', () => {
-    const [data] = createHook(true);
-    const component = html`
-      <div
-        is-list
-        ${{
-          $children: data.$value((bool) => {
-            const el = render(html`<p
-              key="1"
-              ignore="data-state"
-              data-testid="test"
-              ${bool ? 'data-state="test"' : ''}
-            >
-              ${bool}
-            </p>`);
+    const [hook] = createHook(true);
 
-            return [el];
-          }),
-        }}
-      ></div>
-    `;
+    const child = (bool) =>
+      render(html`<i
+        key="test"
+        ignore="data-state"
+        data-testid="foo"
+        ${bool ? 'data-state="test"' : ''}
+      ></i>`);
+    const component = html`<div is-list>${hook.$value(child)}</div>`;
     render(component, 'body');
 
-    const text = screen.getByTestId('test').dataset.state;
-    data.value = false;
+    const { state } = screen.getByTestId('foo').dataset;
+    hook.value = false;
 
-    expect(screen.getByTestId('test')).toHaveAttribute('data-state');
-    expect(screen.getByTestId('test').dataset.state).toBe(text);
+    expect(screen.getByTestId('foo')).toHaveAttribute('data-state');
+    expect(screen.getByTestId('foo').dataset.state).toBe(state);
   });
 
-  it('does not update child with attribute `ignore-all', () => {
-    const [data] = createHook(1);
-    const child = (i) => {
-      const rand = Math.random();
-
-      return html`
-        <div
-          is-text
-          key="${i}"
-          ignore-all
-          data-id="${rand}"
-          data-testid="test-child-${i}"
-        >
-          ${rand}
-        </div>
-      `;
-    };
-    const parent = html`
-      <div is-list>
-        ${data.$value((n) =>
-          new Array(n).fill().map((_, i) => render(child(i)))
+  it('will skip update if child has attribute `ignore-all`', () => {
+    const [hook] = createHook({ list: defaultData });
+    render(
+      html`<ul is-list>
+        ${hook.$list.map((data) =>
+          ListItem(
+            data,
+            data.id === '1'
+              ? {
+                  'is-text': '',
+                  'ignore-all': '',
+                  'data-testid': 'skip',
+                  'data-num': Math.random(),
+                }
+              : {}
+          )
         )}
-      </div>
-    `;
-    render(parent, 'body');
+      </ul>`,
+      'body'
+    );
+    const rand = screen.getByTestId('skip').dataset.num;
 
-    const test = screen.getByTestId('test-child-0');
-    const text = test.textContent.trim();
-    const id = test.dataset.id;
+    hook.list = [
+      { id: '1', text: 'read' },
+      { id: '2', text: 'shower' },
+      { id: '3', text: 'sleep' },
+    ];
 
-    data.value = 2;
-
-    expect(test).toHaveAttribute('data-id', id);
-    expect(test.textContent.trim()).toBe(text);
-  });
-
-  describe('updates children', () => {
-    let listItemMountCallback, listItemUnmountCallback; // eslint-disable-line
-    beforeEach(() => {
-      listItemMountCallback = jest.fn();
-      listItemUnmountCallback = jest.fn();
-    });
-
-    const createCustomTodoItem = (mount, unmount) => (data) => {
-      const defaultLabels = ['physical', 'mental', 'misc'];
-
-      return html`
-        <div
-          class="todo"
-          ${data.keyString}="${data.id}"
-          data-testid="todo-${data.id}"
-          checked="${data.checked || 'false'}"
-        >
-          <div class="todo__labels">
-            <ul is-list>
-              ${(data.labels || defaultLabels).map(
-                (label) =>
-                  html`<li
-                    is-text
-                    ${data.keyString}="${label}"
-                    ${{
-                      onMount: listItemMountCallback,
-                      onUnmount: listItemUnmountCallback,
-                    }}
-                  >
-                    ${label}
-                  </li>`
-              )}
-            </ul>
-          </div>
-          <div class="todo__body" onUnmount=${unmount}>
-            <h2 class="todo__title" onMount=${mount} onUnmount=${unmount}>
-              ${data.name}
-            </h2>
-            <span class="todo__id">${data.id}</span>
-          </div>
-          <div is-text class="todo__date" onUnmount=${unmount}>
-            <span>Due date: </span>${data.dueDate}
-          </div>
-        </div>
-      `;
-    };
-
-    it('but only the node that was actually changed', (done) => {
-      const { data, component } = createTodoList(
-        defaultData,
-        createCustomTodoItem(mountCallback, unmountCallback)
-      );
-      render(component, 'body');
-
-      data.todos = [
-        { id: 'eat', name: 'eat', checked: 'true' },
-        { id: 'shower', name: 'shower' },
-        { id: 'sleep', name: 'deep sleep' },
-      ];
-
-      setTimeout(() => {
-        try {
-          expect(screen.getByTestId('todo-eat')).toHaveAttribute('checked');
-          expect(getTitle(screen.getByTestId('todo-list'))).toBe(
-            data.todos.map((todo) => todo.name).join()
-          );
-          expect(mountCallback).toHaveBeenCalledTimes(3);
-          expect(unmountCallback).not.toHaveBeenCalled();
-          done();
-        } catch (error) {
-          done(error);
-        }
-      }, 0);
-    });
-
-    it('by updating innerHTML if is-text', (done) => {
-      const { data, component } = createTodoList(
-        defaultData,
-        createCustomTodoItem(mountCallback, unmountCallback)
-      );
-      render(component, 'body');
-
-      data.todos = [
-        { id: 'eat', name: 'eat', dueDate: new Date().toString() },
-        { id: 'shower', name: 'shower' },
-        { id: 'sleep', name: 'sleep' },
-      ];
-
-      const el = screen.getByTestId('todo-eat').querySelector('.todo__date');
-
-      setTimeout(() => {
-        try {
-          expect(el).toContainHTML(
-            `<span>Due date: </span>${data.todos[0].dueDate}`
-          );
-          expect(unmountCallback).not.toHaveBeenCalled();
-          done();
-        } catch (error) {
-          done(error);
-        }
-      }, 0);
-    });
-
-    it('by diffing recursively if child is type list', (done) => {
-      const { data, component } = createTodoList(
-        defaultData,
-        createCustomTodoItem(mountCallback, unmountCallback)
-      );
-      render(component, 'body');
-
-      data.todos = [
-        { id: 'eat', name: 'eat', labels: ['physical', 'diet', 'misc'] },
-        { id: 'shower', name: 'shower' },
-        { id: 'sleep', name: 'sleep', labels: ['misc'] },
-      ];
-
-      setTimeout(() => {
-        try {
-          expect(
-            getKeys(
-              screen.getByTestId('todo-eat').querySelector('.todo__labels ul')
-            )
-          ).toBe(data.todos[0].labels.join());
-          expect(
-            getKeys(
-              screen.getByTestId('todo-sleep').querySelector('.todo__labels ul')
-            )
-          ).toBe(data.todos[2].labels.join());
-          expect(unmountCallback).not.toHaveBeenCalled();
-          /**
-             the base should be 9 as there are 9 labels on first render
-            plus 1 for "diet" but instead we get 8
-            which happens when we remove a label without a replacement
-            which is on task "sleep" because we remove 2 labels
-            so I guess what is happening is that 9 + 1 ("diet") - 2 (the removed labels)
-            to check, remove labels for "sleep" and we should get 10
-            this is weird because we get the correct amount of calls for unmount
-            and this could also happen to other test cases
-            commenting out this assertion for now till I figured out the real issue
-           */
-          // expect(listItemMountCallback).toHaveBeenCalledTimes(10);
-          expect(listItemUnmountCallback).toHaveBeenCalledTimes(3);
-          done();
-        } catch (error) {
-          done(error);
-        }
-      }, 0);
-    });
+    expect(screen.getByTestId('skip')).toHaveTextContent('eat');
+    expect(screen.getByTestId('skip')).toHaveAttribute('data-num', rand);
   });
 });
