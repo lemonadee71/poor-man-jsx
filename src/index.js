d68e7b83ac02c500551ca66e53e14d5f9c4f1047
Lemon <riegoshinandrei@gmail.com>
2022-09-20 18:19:02 +0800

    feat: Add disableLifecycle to global settings

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { define, remove } from './custom-components';
 import { addDirective } from './directives';
 import { html, render, apply } from './main';
 import { createHook } from './hooks';
-import { enableLifecycle } from './lifecycle';
+import { enableLifecycle, disableLifecycle } from './lifecycle';
 import { addPreprocessor, removePreprocessor } from './preprocess';
 
 enableLifecycle();
@@ -11,6 +11,7 @@ enableLifecycle();
  * The global settings for poor-man-jsx
  */
 const PoorManJSX = {
+  disableLifecycle,
   plugins: {
     addPreprocessor,
     removePreprocessor,
