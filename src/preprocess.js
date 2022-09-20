b465dcb90944176bc6ce64175a9523bb7844d2b1
Lemon <riegoshinandrei@gmail.com>
2022-09-20 17:24:11 +0800

    refactor: Add removing of preprocessor

--- src/preprocess.js
+++ src/preprocess.js
@@ -1,18 +1,27 @@
 import { compose } from './utils/general';
 import { isArray } from './utils/is';
 
-const templateStringProcessors = [];
+let processors = [];
 
 /**
  * Add a callback that will process the html string before creation
  * @param  {...Function|Function[]} callback
  * @returns
  */
-export const addPreprocessor = (...callback) =>
-  templateStringProcessors.push(...callback);
+export const addPreprocessor = (...callback) => processors.push(...callback);
+
+/**
+ * Remove a processor callback. If callback is omitted,
+ * all processors are removed
+ * @param {?Function} callback
+ */
+export const removePreprocessor = (callback = null) => {
+  if (callback) processors = processors.filter((fn) => fn !== callback);
+  else processors = [];
+};
 
 export const preprocess = (htmlString) =>
-  templateStringProcessors.reduce(
+  processors.reduce(
     (result, fn) => (isArray(fn) ? compose(...fn)(result) : fn(result)),
     htmlString
   );
