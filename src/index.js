b30d7b63a31d740b60d6e43606537bd02708639a
Lemon <riegoshinandrei@gmail.com>
2022-09-20 17:26:56 +0800

    refactor: Expose plugin settings

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { define, remove } from './custom-components';
+import { addDirective } from './directives';
 import { html, render, apply } from './main';
 import { createHook } from './hooks';
 import { enableLifecycle } from './lifecycle';
+import { addPreprocessor, removePreprocessor } from './preprocess';
 
 enableLifecycle();
 
@@ -9,9 +11,11 @@ enableLifecycle();
  * The global settings for poor-man-jsx
  */
 const PoorManJSX = {
-  /**
-   * Settings for custom components
-   */
+  plugins: {
+    addPreprocessor,
+    removePreprocessor,
+    addDirective,
+  },
   customComponents: { define, remove },
 };
 
