dd0ddd4d4e6b4cc1b66bc838db253aeff5b90697
Lemon <riegoshinandrei@gmail.com>
2022-09-20 18:07:07 +0800

    test: Add tests for addDirective

--- tests/plugins.test.js
+++ tests/plugins.test.js
@@ -1,10 +1,8 @@
 import '@testing-library/jest-dom/extend-expect';
 import { screen } from '@testing-library/dom';
-import PoorManJSX, { html, render } from '../src';
+import PoorManJSX, { apply, html, render } from '../src';
 
 describe('preprocessor', () => {
-  beforeEach(() => jest.clearAllMocks());
-
   afterEach(() => {
     document.body.innerHTML = '';
     jest.clearAllMocks();
@@ -28,5 +26,72 @@ describe('preprocessor', () => {
     render(html`<>${children}</>`, 'body');
 
     expect(document.body.children.length).toBe(3);
+  });
+});
+
+describe('addDirective', () => {
+  const directive = {
+    name: 'autosize',
+    type: 'autosize',
+    callback: (element, _, modify) => {
+      modify(element, 'attr', { key: 'data-autosize', value: 'true' });
+      element.removeAttribute(':autosize');
+    },
+  };
+
+  afterEach(() => {
+    document.body.innerHTML = '';
+  });
+
+  it('allow users to add their own directive', () => {
+    const getType = (str) => (str === ':autosize' ? ['autosize'] : null);
+    PoorManJSX.plugins.addDirective({ ...directive, getType });
+
+    render(html`<div :autosize data-testid="autosize"></div>`, 'body');
+
+    expect(screen.getByTestId('autosize')).not.toHaveAttribute(':autosize');
+    expect(screen.getByTestId('autosize')).toHaveAttribute(
+      'data-autosize',
+      'true'
+    );
+  });
+
+  it('allow different keys for attrName and objKey with array', () => {
+    const getTypeFromAttr = (str) =>
+      str === ':autosize' ? ['autosize'] : null;
+    const getTypeFromKey = (str) => (str === 'autosize' ? ['autosize'] : null);
+
+    PoorManJSX.plugins.addDirective({
+      ...directive,
+      getType: [getTypeFromAttr, getTypeFromKey],
+    });
+
+    render(html`<div data-testid="autosize"></div>`, 'body');
+    apply(screen.getByTestId('autosize'), { autosize: true });
+
+    expect(screen.getByTestId('autosize')).not.toHaveAttribute(':autosize');
+    expect(screen.getByTestId('autosize')).toHaveAttribute(
+      'data-autosize',
+      'true'
+    );
+  });
+
+  it('allow different keys for attrName and objKey with object', () => {
+    const attrName = (str) => (str === ':autosize' ? ['autosize'] : null);
+    const objKey = (str) => (str === 'autosize' ? ['autosize'] : null);
+
+    PoorManJSX.plugins.addDirective({
+      ...directive,
+      getType: { attrName, objKey },
+    });
+
+    render(html`<div data-testid="autosize"></div>`, 'body');
+    apply(screen.getByTestId('autosize'), { autosize: true });
+
+    expect(screen.getByTestId('autosize')).not.toHaveAttribute(':autosize');
+    expect(screen.getByTestId('autosize')).toHaveAttribute(
+      'data-autosize',
+      'true'
+    );
   });
 });
